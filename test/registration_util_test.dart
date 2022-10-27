a4c05669e365a8154b6169b00fe7d4c97297d952
Macbook 18 Pro <macbook18pro@Macbooks-MacBook-Pro.local>
2022-10-28 00:10:03 +0700

    luke's unit test week 1

--- test/registration_util_test.dart
+++ test/registration_util_test.dart
@@ -4,13 +4,33 @@ import 'package:flutter_testing/registration_util.dart';
 import 'package:test/test.dart';
 
 void main() {
-  test('empty username returns false', () {
-    expect(
-        RegistrationUtil.instance
-            .validateRegistrationUserInput("", "123", "123"),
-        false);
+  group('Test validate registration user input', () {
+    test('empty username returns false', () {
+      expect(
+          RegistrationUtil.instance
+              .validateRegistrationUserInput("", "123", "123"),
+          false);
+    });
+    // password less than two digits.
+    test('password less than two digits returns false', () {
+      expect(
+          RegistrationUtil.instance
+              .validateRegistrationUserInput("my name", "3", "3"),
+          false);
+    });
+    // confirmed password not the same as password.
+    test('confirmed password not the same as password returns false', () {
+      expect(
+          RegistrationUtil.instance
+              .validateRegistrationUserInput("my name", "123", "023"),
+          false);
+    });
+    // valid user input.
+    test('valid user input returns true', () {
+      expect(
+          RegistrationUtil.instance
+              .validateRegistrationUserInput("my name", "123", "123"),
+          true);
+    });
   });
-  // password less than two digits
-  // confirmed password not the same as password.
-  // valid user input
 }
